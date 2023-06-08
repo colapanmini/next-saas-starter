8bc76d896538de571020f90657f5c7cc53fc3ee0
Lawrence Timothy <colapanmini@gmail.com>
2023-06-08 18:09:49 +0700

    Hero Text

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,15 +14,15 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>the coolest, football club you have ever seen</CustomOverTitle>
+        <Heading>HIGH REACHING FOOTBALL CLUB ACADEMY</Heading>
         <Description>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
           repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
         </Description>
         <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
-            Subscribe to the newsletter <span>&rarr;</span>
+            Sign Up for Free Trial! <span>&rarr;</span>
           </Button>
           <NextLink href="#whitepaper" passHref>
             <Button transparent>
